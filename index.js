d039b9d42bc347b9f930d150740fcd22f1be96af
mgbux <132969773+mgbux@users.noreply.github.com>
2023-05-17 12:06:06 +0800

    undo window thing

--- index.js
+++ index.js
@@ -6,19 +6,17 @@ function homeOne() {
     homeScoreValue += 1
     homeScore.textContent = homeScoreValue
 }
-window.homeOne = homeOne
 
 function homeTwo() {
     homeScoreValue += 2
     homeScore.textContent = homeScoreValue
 }
-window.homeTwo = homeTwo
 
 function homeThree() {
     homeScoreValue += 3
     homeScore.textContent = homeScoreValue
 }
-window.homeThree = homeThree
+
 
 let guestScore = document.getElementById("guest-score")
 let guestScoreValue = 0
@@ -27,19 +25,17 @@ function guestOne() {
     guestScoreValue += 1
     guestScore.textContent = guestScoreValue
 }
-window.guestOne = guestOne
 
 function guestTwo() {
     guestScoreValue += 2
     guestScore.textContent = guestScoreValue
 }
-window.guestTwo = guestTwo
 
 function guestThree() {
     guestScoreValue += 3
     guestScore.textContent = guestScoreValue
 }
-window.homeOne = homeOne
+
 
 function resetScores() {
     homeScoreValue = 0
@@ -47,4 +43,3 @@ function resetScores() {
     guestScoreValue = 0
     guestScore.textContent = guestScoreValue
 }
-window.resetScores = resetScores

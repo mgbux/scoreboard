8edb19b72c9cd5ea738c579607909e6217135a6a
mgbux <132969773+mgbux@users.noreply.github.com>
2023-05-17 12:02:22 +0800

    window thing

--- index.js
+++ index.js
@@ -6,17 +6,19 @@ function homeOne() {
     homeScoreValue += 1
     homeScore.textContent = homeScoreValue
 }
+window.homeOne = homeOne
 
 function homeTwo() {
     homeScoreValue += 2
     homeScore.textContent = homeScoreValue
 }
+window.homeTwo = homeTwo
 
 function homeThree() {
     homeScoreValue += 3
     homeScore.textContent = homeScoreValue
 }
-
+window.homeThree = homeThree
 
 let guestScore = document.getElementById("guest-score")
 let guestScoreValue = 0
@@ -25,17 +27,19 @@ function guestOne() {
     guestScoreValue += 1
     guestScore.textContent = guestScoreValue
 }
+window.guestOne = guestOne
 
 function guestTwo() {
     guestScoreValue += 2
     guestScore.textContent = guestScoreValue
 }
+window.guestTwo = guestTwo
 
 function guestThree() {
     guestScoreValue += 3
     guestScore.textContent = guestScoreValue
 }
-
+window.homeOne = homeOne
 
 function resetScores() {
     homeScoreValue = 0
@@ -43,3 +47,4 @@ function resetScores() {
     guestScoreValue = 0
     guestScore.textContent = guestScoreValue
 }
+window.resetScores = resetScores
